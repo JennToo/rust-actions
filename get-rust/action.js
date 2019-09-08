dae74c028f031364acb7f057c6171ca16a73a6ae
Jennifer Wilcox <jennifer@nitori.org>
2019-09-08 14:52:26 -0500

    Cleanup the spawning logic some

--- get-rust/action.js
+++ get-rust/action.js
@@ -1,81 +1,71 @@
 var spawn = require('child_process').spawn;
+
 const version = process.env.INPUT_VERSION;
 const make_default = process.env.INPUT_MAKE_DEFAULT;
 const components = process.env.INPUT_COMPONENTS;
 
-function install_toolchain() {
-    const proc = spawn("rustup", ["toolchain", "install", version], { "stdio": 'inherit' });
+function spawnAsync(command, args, options) {
+    return new Promise((resolve, reject) => {
+        const proc = spawn(command, args, options);
 
-    proc.on("close", (code) => {
-        if(code != 0) {
-            process.exit(code);
+        proc.on("error", (err) => {
+            reject(err);
+        });
+
+        proc.on("close", (code) => {
+            if(code == 0) {
+                resolve();
+            } else {
+                reject(code);
+            }
+        });
+    });
+}
+
+async function install_toolchain() {
+    await spawnAsync("rustup", ["toolchain", "install", version], { "stdio": 'inherit' });
+}
+
+async function set_as_default() {
+    await spawnAsync("rustup", ["default", version], { "stdio": 'inherit' });
+}
+
+async function install_rustup_and_toolchain() {
+    const command = `curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${version}`
+    const proc = spawnAsync(command, [], { "stdio": "inherit", "shell": true });
+    const new_path = `${process.env.HOME}/.cargo/bin`
+    console.log(`::add-path::${new_path}`);
+    return new_path;
+}
+
+async function install_components(rustup) {
+    if(components == "") {
+        console.log("No components to install");
+    } else {
+        const command = `${rustup} component add --toolchain ${version} ${components}`;
+        await spawnAsync(command, [], { "stdio": "inherit", "shell": true });
+    }
+}
+
+async function main() {
+    try {
+        console.log("::set-env name=RUST_BACKTRACE::1")
+        var rustup = "rustup";
+        if(process.platform === "darwin") {
+            console.log("Detected macOS, installing rustup as well");
+            rustup = await install_rustup_and_toolchain();
+        } else {
+            await install_toolchain();
         }
 
         if(make_default) {
-            set_as_default();
-        } else {
-            install_components("rustup");
+            await set_as_default();
         }
-    });
-
-    proc.on("error", (err) => {
-        console.log(`Error while installing toolchain: ${err}`);
-        process.exit(1)
-    });
-}
-
-function set_as_default() {
-    const proc = spawn("rustup", ["default", version], { "stdio": 'inherit' });
-    proc.on("close", (code) => {
-        if(code != 0) {
-            process.exit(code);
-        }
-
-        install_components("rustup");
-    })
-    proc.on("error", (err) => {
-        console.log(`Error while setting default toolchain: ${err}`);
-        process.exit(1)
-    });
-}
-
-function install_rustup_and_toolchain() {
-    const command = `curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${version}`
-    const proc = spawn(command, [], { "stdio": "inherit", "shell": true });
-    proc.on("close", (code) => {
-        const new_path = `${process.env.HOME}/.cargo/bin`
-        console.log(`::add-path::${new_path}`);
-        if(code != 0) {
-            process.exit(code);
-        }
-        install_components(`${new_path}/rustup`);
-    })
-    proc.on("error", (err) => {
-        console.log(`Error while installing rust: ${err}`);
+        await install_components(rustup);
+    } catch(e) {
+        console.error(e);
         process.exit(1);
-    });
-}
-
-function install_components(rustup) {
-    if(components == "") {
-        console.log("No components to install");
-        process.exit(0);
     }
-    const command = `${rustup} component add --toolchain ${version} ${components}`;
-    const proc = spawn(command, [], { "stdio": "inherit", "shell": true });
-    proc.on("close", (code) => {
-        process.exit(code);
-    })
-    proc.on("error", (err) => {
-        console.log(`Error while installing components: ${err}`);
-        process.exit(1);
-    });
 }
 
-console.log("::set-env name=RUST_BACKTRACE::1")
-if(process.platform === "darwin") {
-    console.log("Detected macOS, installing rustup as well");
-    install_rustup_and_toolchain();
-} else {
-    install_toolchain();
-}
+main();
