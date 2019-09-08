d160a9ceae48ed3904df3a1b63d4faad398060d1
Jennifer Wilcox <jennifer@nitori.org>
2019-09-08 14:01:18 -0500

    Add component support as well

--- get-rust/action.js
+++ get-rust/action.js
@@ -1,6 +1,7 @@
 var spawn = require('child_process').spawn;
 const version = process.env.INPUT_VERSION;
 const make_default = process.env.INPUT_MAKE_DEFAULT;
+const components = process.env.INPUT_COMPONENTS;
 
 function install_toolchain() {
     const proc = spawn("rustup", ["toolchain", "install", version], { "stdio": 'inherit' });
@@ -12,6 +13,8 @@ function install_toolchain() {
 
         if(make_default) {
             set_as_default();
+        } else {
+            install_components("rustup");
         }
     });
 
@@ -24,7 +27,11 @@ function install_toolchain() {
 function set_as_default() {
     const proc = spawn("rustup", ["default", version], { "stdio": 'inherit' });
     proc.on("close", (code) => {
-        process.exit(code);
+        if(code != 0) {
+            process.exit(code);
+        }
+
+        install_components("rustup");
     })
     proc.on("error", (err) => {
         console.log(`Error while setting default toolchain: ${err}`);
@@ -36,11 +43,31 @@ function install_rustup_and_toolchain() {
     const command = `curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain ${version}`
     const proc = spawn(command, [], { "stdio": "inherit", "shell": true });
     proc.on("close", (code) => {
-        console.log(`::add-path::${process.env.HOME}/.cargo/bin`);
-        process.exit(code);
+        const new_path = `${process.env.HOME}/.cargo/bin`
+        console.log(`::add-path::${new_path}`);
+        if(code != 0) {
+            process.exit(code);
+        }
+        install_components(`${new_path}/rustup`);
     })
     proc.on("error", (err) => {
         console.log(`Error while installing rust: ${err}`);
+        process.exit(1);
+    });
+}
+
+function install_components(rustup) {
+    if(components == "") {
+        console.log("No components to install");
+        process.exit(0);
+    }
+    const command = `rustup component add --toolchain ${version} ${components}`;
+    const proc = spawn(command, [], { "stdio": "inherit", "shell": true });
+    proc.on("close", (code) => {
+        process.exit(code);
+    })
+    proc.on("error", (err) => {
+        console.log(`Error while installing components: ${err}`);
         process.exit(1);
     });
 }
